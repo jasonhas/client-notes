4300d7b8a20730d8c9a5e39d44cb5fdd17c9bcbe
Jason Hasselkus <jason.hasselkus@gmail.com>
2024-08-17 21:19:09 -0500

    updates styles

--- app/clients/page.jsx
+++ app/clients/page.jsx
@@ -16,10 +16,11 @@ export default function Clients() {
       ]
 
     return (
-        <div>
+        <div className="max-w-6xl mx-auto py-8">
 
             <section>
-            <h2 className="font-semibold text-xl">Client List</h2>
+            <h2 className="font-semibold text-2xl text-center">Client List</h2>
+            
                 <ul>
                 {clients.map((client) => {
                     return (<li><a href={`/clients/${client.id}`}>{client.name}</a></li>)
@@ -30,7 +31,7 @@ export default function Clients() {
             </section>
         </div>
         
-
+ 
         
     )
 }
--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="max-w-6xl mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
         <div className="bg-slate-300 border border-spacing-1 border-slate-500 rounded-xl shadow-xl">
-          <h2 className="text-2xl font-bold text-center">To Do's</h2>
+          <h2 className="text-2xl font-bold text-center p-6">To Do's</h2>
           <ul className="p-8">
             <li>Update Sales Force</li>
             <li>Strategy Document for UPS</li>
@@ -14,8 +14,8 @@ export default function Home() {
             <li>Reply to Dean about additional accounts</li>
           </ul>
         </div>
-        <div className="bg-yellow-50 bg-slate-300 border border-spacing-1 border-slate-500 rounded-xl">
-          <h2 className="text-2xl font-bold text-center">Upcoming Events</h2>
+        <div className="bg-yellow-50 border border-spacing-1 border-slate-500 rounded-xl">
+          <h2 className="text-2xl font-bold text-center p-6">Upcoming Events</h2>
           <ul className="steps steps-vertical p-8">
             <li className="step step-primary">Register</li>
             <li className="step step-primary">Choose plan</li>
